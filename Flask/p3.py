c03eaf045c288d5db72fe46439d9e4765611c5db
makelele29 <jacastillo.ugr@gmail.com>
2016-11-02 18:48:13 +0100

    Practica 3

--- Flask/p3.py
+++ Flask/p3.py
@@ -1,18 +1,40 @@
 # -*- coding: utf-8 -*-
-from flask import Flask,request, url_for,render_template
+from flask import Flask,request, url_for,render_template,session,redirect
 
 app = Flask(__name__)
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
+def ini(text,link):
+    if not session:
+        session['count']=0
+        session['pag']=[{'text':'','link':''},{'text':'','link':''},{'text':'','link':''}]
+    session['pag'][session['count']]['text']=text
+    session['pag'][session['count']]['link']=link
+    session['count']=(session['count']+1)%3
 
 @app.route("/")
 def hello():
-    conjunto={'logo':'logo.png','titulo':'Geoke','subtitulo':'el subtitulo','menu':[{'text':'home','link':'#home'},{'text':'info','link':'#info'},{'text':'contacto','link':'#contacto'}]}
+    ini("Home","/")
+    conjunto={'subtitulo':'Pagina Principal','paginas':session['pag']}
     return render_template('index.html',contenido=conjunto)
 
-@app.route("/fanta")
-def fanta():
-    conjunto={'logo':'fanta.png','titulo':'Fanta','subtitulo':'Ahora con limon','menu':[{'text':'Home','link':'#fanta'},{'text':'Proyecto Fanta','link':'http://www.fanta.com.ar/es/home/'}]}
-    return render_template('index.html',contenido=conjunto)
+@app.route("/registro")
+def registro():
+    ini("Registrarse","/registro")
+    conjunto={'subtitulo':'registrarse','paginas':session['pag']}
+    return render_template('registro.html',contenido=conjunto)
 
+@app.route("/login", methods = ['POST'])
+def login():
+    usu=str(request.form['username'])
+    passwd=str(request.form['pass'])
+    if usu=="dai" and passwd=="dai":
+        session['username']=usu
+    return redirect(url_for('hello'))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('hello'))
 
 @app.errorhandler(404)
 def page_not_found(error):
--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,10 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
- 
+
 VAGRANTFILE_API_VERSION = "2"
- 
+
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
- 
+
   # Insertar ubuntu 16 en la maquina virtual
   config.vm.box = "bento/ubuntu-16.04"
   # Redireccionamiento de puertos para luego probar la web
@@ -13,10 +13,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.synced_folder "htdocs", "/var/www/html"
   config.vm.synced_folder "Ejercicios", "/Ejercicios"
   config.vm.synced_folder "Flask", "/Flask"
-  # configuración de LAMP
-  config.vm.provision "shell", path: "config.sh"
   config.vm.provision "shell", path: "flask.sh"
-  
+
   config.vm.provider "virtualbox" do |v|
     # Para que tenga entorno visual
 	# v.gui = true
